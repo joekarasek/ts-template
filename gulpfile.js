7c9c2b378e26ca56bca2adf6121b9c3d29e4a6a4
Joe Karasek <joseph.karasek@gmail.com>
2017-02-17 11:06:05 -0800

    update to new gulp handled scss compiling, including autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 'use strict';
 const gulp = require('gulp');
 const config = require('rc')('p2-theme-core', require('./gulpconfig.js'));
-// const tsThemeCore = require('./scripts/gulp/index.js');
+const tsThemeCore = require('./scripts/gulp/index.js');
 
-console.log(config);
 const tasks = {
   compile: [],
   watch: [],
@@ -12,17 +11,19 @@ const tasks = {
   default: [],
 };
 //
-// tsThemeCore(gulp, config, tasks);
+tsThemeCore(gulp, config, tasks);
+
+console.log(tasks);
 //
-// gulp.task('clean', gulp.parallel(tasks.clean));
-// gulp.task('compile', gulp.series(
-//     'clean',
-//     gulp.series(tasks.compile)
-// ));
-// gulp.task('validate', gulp.parallel(tasks.validate));
-// gulp.task('watch', gulp.parallel(tasks.watch));
-// tasks.default.push('watch');
-// gulp.task('default', gulp.series(
-//     'compile',
-//     gulp.parallel(tasks.default)
-// ));
+gulp.task('clean', gulp.parallel(tasks.clean));
+gulp.task('compile', gulp.series(
+    'clean',
+    gulp.series(tasks.compile)
+));
+gulp.task('validate', gulp.parallel(tasks.validate));
+gulp.task('watch', gulp.parallel(tasks.watch));
+tasks.default.push('watch');
+gulp.task('default', gulp.series(
+    'compile',
+    gulp.parallel(tasks.default)
+));
---
+++ scripts/gulp/index.js
@@ -0,0 +1,41 @@
+'use strict';
+const _ = require('lodash');
+const defaultConfig = require('./config.default');
+
+module.exports = (gulp, userConfig, tasks) => {
+  const config = _.merge(defaultConfig, userConfig);
+
+  // /* eslint-disable global-require */
+  // if (config.browserSync.enabled) {
+  //   require('./lib/browser-sync')(gulp, config, tasks);
+  // }
+  //
+  // if (config.icons.enabled) {
+  //   require('./lib/icons')(gulp, config, tasks);
+  // }
+  //
+  // if (config.js.enabled) {
+  //   require('./lib/js')(gulp, config, tasks);
+  // }
+  //
+  if (config.css.enabled) {
+    require('./css')(gulp, config, tasks);
+  }
+  //
+  // if (config.patternLab.enabled) {
+  //   require('./lib/pattern-lab--php-twig')(gulp, config, tasks);
+  // }
+  //
+  // if (config.drupal.enabled) {
+  //   require('./lib/drupal')(gulp, config, tasks);
+  // }
+  // /* eslint-enable global-require */
+
+  // Instead of `gulp.parallel`, which is what is set in Pattern Lab Starter's `gulpfile.js`, this
+  // uses `gulp.series`. Needed to help with the Gulp task dependencies lost going from v3 to v4.
+  // We basically need icons compiled before CSS & CSS/JS compiled before inject:pl before pl
+  // compile. The order of the `require`s above is the order that compiles run in; not perfect, but
+  // it works.
+  // eslint-disable-next-line no-param-reassign
+  tasks.compile = gulp.series(tasks.compile);
+};
