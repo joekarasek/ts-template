f8d71d293764ced346d6e1ab510204dc813719ba
Joe Karasek <joseph.karasek@gmail.com>
2017-03-01 14:21:37 -0800

    import gulp 4 gulpfile and create hwdGulpCore directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,34 @@
+'use strict';
+const gulp        = require('gulp');
+const config      = require('./gulpconfig.js');
+const hwdGulpCore = require('./scripts/hwdGulpCore/index.js');
+
+const tasks = {
+  compile: [],
+  watch: [],
+  validate: [],
+  clean: [],
+  default: [],
+};
+
+hwdGulpCore(gulp, config, tasks);
+
+console.log(config);
+
+gulp.task('clean', gulp.parallel(tasks.clean));
+gulp.task('compile', gulp.series(
+    'clean',
+    gulp.series(tasks.compile)
+));
+gulp.task('validate', gulp.parallel(tasks.validate));
+gulp.task('watch', gulp.parallel(tasks.watch));
+tasks.default.push('watch');
+gulp.task('default', gulp.series(
+    'compile',
+    gulp.parallel(tasks.default)
+));
+
+
 const browserSync = require('browser-sync');
 const cache = require('gulp-cache');
 const cssnano = require('gulp-cssnano');
