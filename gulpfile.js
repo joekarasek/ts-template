9a2cefc85d5bfd8936552a80caf09be6ebe767f0
Joe Karasek <joseph.karasek@gmail.com>
2017-03-01 15:06:09 -0800

    add basic regions to the gulp config file

--- gulpfile.js
+++ gulpfile.js
@@ -29,155 +29,154 @@ gulp.task('default', gulp.series(
 ));
 
 
-const browserSync = require('browser-sync');
-const cache = require('gulp-cache');
-const cssnano = require('gulp-cssnano');
-const config = require('./gulpconfig.json');
-const del = require('del');
-const fs = require('fs');
-const gulp = require('gulp');
-const gulpIf = require('gulp-if');
-const jshint = require('gulp-jshint');
-const htmlhint = require('gulp-htmlhint');
-const imagemin = require('gulp-imagemin');
-const inject = require('gulp-inject-string');
-const merge = require('merge-stream');
-const prompt = require('prompt');
-const rename = require('gulp-rename');
-const runSequence = require('run-sequence');
-const sass = require('gulp-sass');
-const spritesmith = require('gulp.spritesmith');
-const useref = require('gulp-useref');
-const uglify = require('gulp-uglify');
-
-
-
-/******* Tasks **********/
-gulp.task('browserSync', function() { // Initiate BrowserSync
-  browserSync.init({
-    server: {
-      baseDir: config.baseDir
-    },
-  })
-});
-
-gulp.task('sass', function(){
-  return gulp.src(config.mainScss)   // Converts main.scss to css file.
-      .pipe(sass())
-      .pipe(gulp.dest(config.baseDir + 'css'))
-      .pipe(browserSync.reload({
-        stream: true
-      }));
-});
-
-gulp.task('autoprefixer', function () {
-  var postcss      = require('gulp-postcss');
-  var sourcemaps   = require('gulp-sourcemaps');
-  var autoprefixer = require('autoprefixer');
-
-  return gulp.src(config.baseDir + 'css/main.css')
-      .pipe(sourcemaps.init())
-      .pipe(postcss([ autoprefixer() ]))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(config.baseDir + 'css/'));
-});
-
-gulp.task('sass-prefix', function() {runSequence('sass', 'autoprefixer')});
-
-gulp.task('useref', function() {                // Useref is used for concatinating between two snippets in index.html file.
-  return gulp.src(config.baseDir + 'index.html')
-  // Concats JS files & CSS Files in between snippets in index.html
-      .pipe(useref())
-      // Minifies only if a JS file
-      .pipe(gulpIf('*.js', uglify()))
-      //Minifies only if a CSS file
-      .pipe(gulpIf('*.css', cssnano()))
-      .pipe(inject.before('<script src="js/main.min.js"></script>', config.snippets.scriptTag))
-      .pipe(inject.before('<link href="css/main.css" rel="stylesheet">', config.snippets.cssTag))
-      .pipe(gulp.dest(config.build));
-});
-
-gulp.task('images', function() {        // Compresses all images.
-  gulp.src(config.baseDir + 'images/**/*.+(jpeg|jpg|gif|svg|png)')
-      .pipe(cache(imagemin({          // Caching checks if already compressed. If so, skips image.
-        interlaced: true
-      })))
-      .pipe(gulp.dest(config.build + '/images'));    // Moves all images to distribution.
-});
-
-gulp.task('fonts', function() { // Moves all font files over to dist
-  gulp.src(config.baseDir+'fonts/**/*')
-      .pipe(gulp.dest(config.build+'fonts'));
-  return gulp.src(config.baseDir+'css/fonts/**/*')
-      .pipe(gulp.dest(config.build+'css/fonts'));
-});
-
-gulp.task('lint', function() {
-  return gulp.src(config.baseDir + 'js/custom.js')
-      .pipe(jshint())
-      .pipe(jshint.reporter('default', { verbose: true }));
-});
-
-
-// gulp.task('sprite', function () {
-//     del.sync(config.baseDir + 'css/sprite.css');
-//     del.sync(config.baseDir + 'images/sprite.png');
+// const cache = require('gulp-cache');
+// const cssnano = require('gulp-cssnano');
+// const config = require('./gulpconfig.json');
+// const del = require('del');
+// const fs = require('fs');
+// const gulp = require('gulp');
+// const gulpIf = require('gulp-if');
+// const jshint = require('gulp-jshint');
+// const htmlhint = require('gulp-htmlhint');
+// const imagemin = require('gulp-imagemin');
+// const inject = require('gulp-inject-string');
+// const merge = require('merge-stream');
+// const prompt = require('prompt');
+// const rename = require('gulp-rename');
+// const runSequence = require('run-sequence');
+// const sass = require('gulp-sass');
+// const spritesmith = require('gulp.spritesmith');
+// const useref = require('gulp-useref');
+// const uglify = require('gulp-uglify');
 //
-//     // Generate our spritesheet for png files
-//     var spriteData = gulp.src(config.baseDir+'images/sprite/*.png').pipe(spritesmith({
-//         imgName: 'sprite.png',
-//         cssName: 'sprite.css'
-//     }));
-//     var imgStream = spriteData.img
-//         .pipe(gulp.dest(config.baseDir+'images/'), (config.build+'css/'))
-//         .pipe(gulp.dest(config.baseDir+'images/'))
-//         .pipe(gulp.dest(config.build+'images/'));// Destination for sprite PNG
-//     var cssStream = spriteData.css
-//         .pipe(gulp.dest(config.baseDir+'css/')); // Destination for sprite.scss
-//     gulp.src(config.baseDir+'index.html')
-//         .pipe(inject.before('<!--endbuild-->', '<link href="css/sprite.css" rel="stylesheet">'))
-//         .pipe(gulp.dest(config.baseDir));
+//
+//
+// /******* Tasks **********/
+// gulp.task('browserSync', function() { // Initiate BrowserSync
+//   browserSync.init({
+//     server: {
+//       baseDir: config.baseDir
+//     },
+//   })
 // });
-
-
-gulp.task('watch', ['sass', 'browserSync'], function(){           // Runs both browsersync and sass-prefix concurrently
-  gulp.watch(config.baseDir+'/scss/**/*.+(scss|sass)', ['sass-prefix']);
-  gulp.watch(config.baseDir+'*.html', browserSync.reload);
-  gulp.watch(config.baseDir+'js/custom.js', ['lint']);
-  gulp.watch(config.baseDir+'js/**/*.js', browserSync.reload);
-  gulp.watch(config.baseDir+'images/**/*.+(jpg|gif|svg|jpeg|png)', ['images']);
-  // gulp.watch(config.baseDir+'images/*.png', ['sprite']); //Spriting currently disabled.
-});
-
-
-
-gulp.task('valid', function () {
-  gulp.src(config.baseDir+'index.html')
-      .pipe(htmlhint())
-      .pipe(htmlhint.reporter('htmlhint-stylish'));
-});
-
-
-gulp.task('build', function (callback) {
-  runSequence('clean:dist', 'sass-prefix', ['valid', 'lint', 'useref', 'images', 'fonts'], // If want spriting, must add in task 'sprite'.
-      callback
-  );
-});
-
-gulp.task('default', function (callback) {
-  runSequence(['sass-prefix', 'valid', 'browserSync', 'watch'],
-      callback
-  );
-});
-
-gulp.task('clean:dist', function() { // Completely deletes dist folder
-  return del.sync(config.build);
-});
-
-
-gulp.task('cache:clear', function(callback) { // Clears cache of project
-  return cache.clearAll(callback)
-});
+//
+// gulp.task('sass', function(){
+//   return gulp.src(config.mainScss)   // Converts main.scss to css file.
+//       .pipe(sass())
+//       .pipe(gulp.dest(config.baseDir + 'css'))
+//       .pipe(browserSync.reload({
+//         stream: true
+//       }));
+// });
+//
+// gulp.task('autoprefixer', function () {
+//   var postcss      = require('gulp-postcss');
+//   var sourcemaps   = require('gulp-sourcemaps');
+//   var autoprefixer = require('autoprefixer');
+//
+//   return gulp.src(config.baseDir + 'css/main.css')
+//       .pipe(sourcemaps.init())
+//       .pipe(postcss([ autoprefixer() ]))
+//       .pipe(sourcemaps.write('.'))
+//       .pipe(gulp.dest(config.baseDir + 'css/'));
+// });
+//
+// gulp.task('sass-prefix', function() {runSequence('sass', 'autoprefixer')});
+//
+// gulp.task('useref', function() {                // Useref is used for concatinating between two snippets in index.html file.
+//   return gulp.src(config.baseDir + 'index.html')
+//   // Concats JS files & CSS Files in between snippets in index.html
+//       .pipe(useref())
+//       // Minifies only if a JS file
+//       .pipe(gulpIf('*.js', uglify()))
+//       //Minifies only if a CSS file
+//       .pipe(gulpIf('*.css', cssnano()))
+//       .pipe(inject.before('<script src="js/main.min.js"></script>', config.snippets.scriptTag))
+//       .pipe(inject.before('<link href="css/main.css" rel="stylesheet">', config.snippets.cssTag))
+//       .pipe(gulp.dest(config.build));
+// });
+//
+// gulp.task('images', function() {        // Compresses all images.
+//   gulp.src(config.baseDir + 'images/**/*.+(jpeg|jpg|gif|svg|png)')
+//       .pipe(cache(imagemin({          // Caching checks if already compressed. If so, skips image.
+//         interlaced: true
+//       })))
+//       .pipe(gulp.dest(config.build + '/images'));    // Moves all images to distribution.
+// });
+//
+// gulp.task('fonts', function() { // Moves all font files over to dist
+//   gulp.src(config.baseDir+'fonts/**/*')
+//       .pipe(gulp.dest(config.build+'fonts'));
+//   return gulp.src(config.baseDir+'css/fonts/**/*')
+//       .pipe(gulp.dest(config.build+'css/fonts'));
+// });
+//
+// gulp.task('lint', function() {
+//   return gulp.src(config.baseDir + 'js/custom.js')
+//       .pipe(jshint())
+//       .pipe(jshint.reporter('default', { verbose: true }));
+// });
+//
+//
+// // gulp.task('sprite', function () {
+// //     del.sync(config.baseDir + 'css/sprite.css');
+// //     del.sync(config.baseDir + 'images/sprite.png');
+// //
+// //     // Generate our spritesheet for png files
+// //     var spriteData = gulp.src(config.baseDir+'images/sprite/*.png').pipe(spritesmith({
+// //         imgName: 'sprite.png',
+// //         cssName: 'sprite.css'
+// //     }));
+// //     var imgStream = spriteData.img
+// //         .pipe(gulp.dest(config.baseDir+'images/'), (config.build+'css/'))
+// //         .pipe(gulp.dest(config.baseDir+'images/'))
+// //         .pipe(gulp.dest(config.build+'images/'));// Destination for sprite PNG
+// //     var cssStream = spriteData.css
+// //         .pipe(gulp.dest(config.baseDir+'css/')); // Destination for sprite.scss
+// //     gulp.src(config.baseDir+'index.html')
+// //         .pipe(inject.before('<!--endbuild-->', '<link href="css/sprite.css" rel="stylesheet">'))
+// //         .pipe(gulp.dest(config.baseDir));
+// // });
+//
+//
+// gulp.task('watch', ['sass', 'browserSync'], function(){           // Runs both browsersync and sass-prefix concurrently
+//   gulp.watch(config.baseDir+'/scss/**/*.+(scss|sass)', ['sass-prefix']);
+//   gulp.watch(config.baseDir+'*.html', browserSync.reload);
+//   gulp.watch(config.baseDir+'js/custom.js', ['lint']);
+//   gulp.watch(config.baseDir+'js/**/*.js', browserSync.reload);
+//   gulp.watch(config.baseDir+'images/**/*.+(jpg|gif|svg|jpeg|png)', ['images']);
+//   // gulp.watch(config.baseDir+'images/*.png', ['sprite']); //Spriting currently disabled.
+// });
+//
+//
+//
+// gulp.task('valid', function () {
+//   gulp.src(config.baseDir+'index.html')
+//       .pipe(htmlhint())
+//       .pipe(htmlhint.reporter('htmlhint-stylish'));
+// });
+//
+//
+// gulp.task('build', function (callback) {
+//   runSequence('clean:dist', 'sass-prefix', ['valid', 'lint', 'useref', 'images', 'fonts'], // If want spriting, must add in task 'sprite'.
+//       callback
+//   );
+// });
+//
+// gulp.task('default', function (callback) {
+//   runSequence(['sass-prefix', 'valid', 'browserSync', 'watch'],
+//       callback
+//   );
+// });
+//
+// gulp.task('clean:dist', function() { // Completely deletes dist folder
+//   return del.sync(config.build);
+// });
+//
+//
+// gulp.task('cache:clear', function(callback) { // Clears cache of project
+//   return cache.clearAll(callback)
+// });
 
 
 
