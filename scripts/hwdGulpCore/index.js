805f066dcf3aba33e8eea69e4cb6ab8a6236b77b
Joe Karasek <joseph.karasek@gmail.com>
2017-03-01 15:05:38 -0800

    add basic conditionals for broad regions of the config file

---
+++ scripts/hwdGulpCore/index.js
@@ -0,0 +1,27 @@
+'use strict';
+const browserSync = require('browser-sync');
+
+module.exports = (gulp, userConfig, tasks) => {
+
+  if (config.browserSync.enabled) {
+    require('./browserSync')(gulp, config, tasks);
+  }
+  
+  if (config.css.enabled) {
+    require('./css')(gulp, config, tasks);
+  }
+
+  if (config.js.enabled) {
+    require('./js')(gulp, config, tasks);
+  }
+
+  if (config.validate.enabled) {
+    require('./validate')(gulp, config, tasks);
+  }
+
+
+  if (config.docs.enabled) {
+    require('./docs')(gulp, config, tasks);
+  }
+  
+};
