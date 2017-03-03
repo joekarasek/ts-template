0147e4deb0e63340607461f0da3a401e61f60c15
Joe Karasek <joseph.karasek@gmail.com>
2017-03-02 20:50:59 -0800

    comment out unbuilt section calls

--- scripts/hwdGulpCore/index.js
+++ scripts/hwdGulpCore/index.js
@@ -3,24 +3,24 @@ const browserSync = require('browser-sync');
 
 module.exports = (gulp, config, tasks) => {
 
-  if (config.browserSync.enabled) {
-    require('./browserSync')(gulp, config, tasks);
-  }
+  // if (config.browserSync.enabled) {
+  //   require('./browserSync')(gulp, config, tasks);
+  // }
   
   if (config.css.enabled) {
     require('./css')(gulp, config, tasks);
   }
-
-  if (config.js.enabled) {
-    require('./js')(gulp, config, tasks);
-  }
-
-  if (config.validate.enabled) {
-    require('./validate')(gulp, config, tasks);
-  }
-  
-  if (config.docs.enabled) {
-    require('./docs')(gulp, config, tasks);
-  }
-  
+  //
+  // if (config.js.enabled) {
+  //   require('./js')(gulp, config, tasks);
+  // }
+  //
+  // if (config.validate.enabled) {
+  //   require('./validate')(gulp, config, tasks);
+  // }
+  //
+  // if (config.docs.enabled) {
+  //   require('./docs')(gulp, config, tasks);
+  // }
+  //
 };
